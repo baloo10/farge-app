8942fd57e490704d90a7cce6f89fd8556799587b
KrigerTre <haugesrede@haugesrede.local>
2020-11-06 14:07:45 +0100

    Add snackbar and styled select

---
+++ src/Navbar.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import "./Navbar.css";
+
+
+class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {format: "hex", open: false};
+        this.handleFormatChange = this.handleFormatChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
+    }
+
+    handleFormatChange(e){
+        this.setState({format: e.target.value, open: true});
+        this.props.handleChange(e.target.value);
+    }
+
+    closeSnackbar(e, reason){
+        //this.setState({open: false})
+
+        if (reason === 'clickaway') {
+            return;
+          }
+          this.setState({ open: false });
+    }
+    render() {
+        //getting the values from props and state
+        const {level, changeLevel} = this.props;
+        const {format} = this.state;
+        return (
+           <header className="Navbar">
+               <div className="logo">
+                   <a href="/">React Farge-verktøy</a>
+               </div>
+               <div className="slider-container">
+                   <span>Farge nivå: {level}</span>
+                <div className="slider">
+                        <Slider 
+                        defaultValue={level} 
+                        min={100} 
+                        max={900}
+                        step={100} 
+                        onAfterChange={changeLevel}
+                        /> 
+                    </div>
+               </div>
+               <div className="select-container">
+                   <Select value={format} onChange={this.handleFormatChange}>
+                       <MenuItem value="hex"> HEX - #ffffff</MenuItem>
+                       <MenuItem value="rgb"> RGB - rgb(255,255,255)</MenuItem>
+                       <MenuItem value="rgba"> RGBA - rgb(255,255,255, 1.0)</MenuItem>
+                   </Select>
+               </div>
+               <Snackbar 
+                anchorOrigin={{ vertical: "bottom", horizontal: "left"}} 
+                open={this.state.open}
+                autoHideDuration={3000}
+                message={<span id="message-id">Format endret til: {format.toUpperCase()}</span>}
+                ContentProps={{
+                    "aria-describedby": "message-id"
+                }}
+                onClose={this.closeSnackbar}
+                action={[
+                    <IconButton 
+                        onClick={this.closeSnackbar} 
+                        color="inherit" 
+                        key="close"
+                        aria-label="close"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+                />
+           </header>
+        )
+    }
+
+}
+
+export default Navbar;
