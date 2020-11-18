993ba12ffa4cb0e32a5d2b84c2709a60b39c6579
KrigerTre <haugesrede@haugesrede.local>
2020-11-18 15:33:42 +0100

    Refactor Navbar Styles and Remove Css

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,11 @@ import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import "./Navbar.css";
+//import "./Navbar.css";
+import {withStyles} from "@material-ui/styles";
+import styles from "./styles/NavbarStyles";
+
+
 
 
 class Navbar extends Component {
@@ -33,18 +37,18 @@ class Navbar extends Component {
     }
     render() {
         //getting the values from props and state
-        const {level, changeLevel, showingAllColors} = this.props;
+        const {level, changeLevel, showingAllColors, classes} = this.props;
         const {format} = this.state;
         return (
-           <header className="Navbar">
-               <div className="logo">
+           <header className={classes.Navbar}>
+               <div className={classes.logo}>
                    <Link to="/">React Farge verktøy</Link>
                    {/* <a href="/">React Farge-verktøy</a> */}
                </div>
                {showingAllColors && (
-               <div className="slider-container">
+               <div>
                    <span>Farge nivå: {level}</span>
-                <div className="slider">
+                <div className={classes.slider}>
                         <Slider 
                         defaultValue={level} 
                         min={100} 
@@ -55,7 +59,7 @@ class Navbar extends Component {
                     </div>
                </div>
                )}
-               <div className="select-container">
+               <div className={classes.selectContainer}>
                    <Select value={format} onChange={this.handleFormatChange}>
                        <MenuItem value="hex"> HEX - #ffffff</MenuItem>
                        <MenuItem value="rgb"> RGB - rgb(255,255,255)</MenuItem>
@@ -88,4 +92,4 @@ class Navbar extends Component {
 
 }
 
-export default Navbar;
+export default withStyles (styles)(Navbar);
