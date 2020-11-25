9ce8e26b748e26d9d4e1e36fcee833893741be89
KrigerTre <haugesrede@haugesrede.local>
2020-11-25 13:42:22 +0100

    Add bg svg to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import {CSSTransition, TransitionGroup,} from 'react-transition-group';
 import MiniPalette from "./MiniPalette";
 import {withStyles} from "@material-ui/styles";
 import styles from "./styles/PaletteListStyles";
@@ -20,16 +21,24 @@ class PaletteList extends Component {
                         <h1 className={classes.heading}> React colors</h1>
                         <Link to="/palette/new">Lag nye farger</Link>   
                     </nav>
-                    <div className={classes.palettes}>
+                    
+                        <TransitionGroup className={classes.palettes}>
                         {palettes.map(palette => (
-                        <MiniPalette {...palette} 
-                        handleClick={() => this.goToPalette(palette.id)}
-                        handleDelete={deletePalette} 
-                        key={palette.id}
-                        id={palette.id}
+                        <CSSTransition 
+                            key={palette.id} 
+                            classNames="fade" 
+                            timeout={500}
+                            >
+                            <MiniPalette {...palette} 
+                            handleClick={() => this.goToPalette(palette.id)}
+                            handleDelete={deletePalette} 
+                            key={palette.id}
+                            id={palette.id}
                             />
+                        </CSSTransition>
                         ))}
-                    </div>
+                        </TransitionGroup>
+                    
                 </div>
             </div>
         );
