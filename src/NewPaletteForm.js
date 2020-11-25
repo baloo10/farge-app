7a2d0231c63339866670ac6f44497abd0662d0d3
KrigerTre <haugesrede@haugesrede.local>
2020-11-25 16:35:12 +0100

    Fix delete dragging bug

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -175,6 +175,7 @@ class NewPaletteForm extends Component {
                 removeColor={this.removeColor} 
                 axis="xy"
                 onSortEnd={this.onSortEnd}
+                distance={20}//the drag will not count as a drag, unless you drag the square 20px
                 />
           </main>
         </div>
